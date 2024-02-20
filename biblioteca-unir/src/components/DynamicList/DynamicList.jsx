205d9c7a5d5f49529f75cb64e8a8034fb3471ee4
JR-Desarrollo <jrdesarrollo.2494@gmail.com>
2024-02-19 22:46:29 -0500

    Ajuste visual

--- biblioteca-unir/src/components/DynamicList/DynamicList.jsx
+++ biblioteca-unir/src/components/DynamicList/DynamicList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import FontIcon from '../FontIcon/FontIcon';
 import { heartIcon, eye, circleCheck, circleXmark } from '../FontIcon/iconConfig';
@@ -17,6 +17,19 @@ const DynamicList = () => {
       ];
   const itemsPerPage = 3;
   const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8762/microservice-search/book/GetAll")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -37,45 +50,71 @@ const DynamicList = () => {
   };
 
   return (
+
     <div>
       <ul className='containerDynamicList'>
-        {currentItems.map((item, index) => (
-          <li key={index}>
+        {data?.map((item) =>(
+          <li key={item.id}>
             <div className='row center-items containerDynamicList__item mt-4 mb-4'>
-                <div className='col-md-2 center-items align-items-center'>
-                    <img className='bookFront' src={item.ruta} alt='portada-libro'/> 
+              <div className='col-md-2 center-items align-items-center'>
+                <img className='bookFront' src="/img/bookFront/LibroPortada1.jpg" alt='portada-libro' />
+              </div>
+              <div className='col-md-8 libros'>
+                <div className='col-md-12'>
+                  <label className='libros__titulo'>{item.qualification}</label>
                 </div>
-                <div className='col-md-8 libros'>
-                    <div className='col-md-12'>
-                        <label className='libros__titulo'>{item.titulo}</label>
-                    </div>
-                    <div className='col-md-12'>
-                        <label className='libros__autor'>{item.autor}</label>
-                    </div>
-                    <div className='col-md-12'>
-                        <p className='libros__descripcion'>{item.description}</p>
-                    </div>
-                    <div className='col-md-auto col-sm-12'>
-                        <span>{item.cantFavoritos} <FontIcon icon={heartIcon} color="var(--color-rojo)"/> </span>
-                        <span>{item.cantVistas} <FontIcon icon={eye} color="var(--colo-azul)"/> | </span>
-                        <label>{item.paginas} Pág. | {item.estado === "Disponible" ? <span className='disponible'>{item.estado} <FontIcon icon={circleCheck} color="var(--colo-verde)"/> </span> : <span className='enPrestamo'>{item.estado} <FontIcon icon={circleXmark} color="var(--colo-rojo)"/>  </span>}</label>
-                    </div>
+                <div className='col-md-12'>
+                  <label className='libros__autor'>{item.author.name}</label>
                 </div>
-                <div className='col-md-2 center-items align-items-center'>
-                    <Link to={'/servioalquiler'}>
-                        <Button id={item.id} classStyle={"btnStyle"} valor={"¡Alquilar!"} onClick={() => almacenarData(item)} isDisabled={item.estado === "Disponible" ? false : true}/>
-                    </Link>
+                <div className='col-md-12'>
+                  <p className='libros__descripcion'>{item.synopsis}</p>
                 </div>
-            </div>            
+              </div>
+            </div>
           </li>
         ))}
       </ul>
-      <div id='pagination'>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
-            <Button classStyle={"btnStyle"} key={pageNumber} onClick={() => handlePaginationClick(pageNumber)} valor={pageNumber}/>
-        ))}
-      </div>
     </div>
+
+    // <div>
+    //   <ul className='containerDynamicList'>
+    //     {currentItems.map((item, index) => (
+    //       <li key={index}>
+    //         <div className='row center-items containerDynamicList__item mt-4 mb-4'>
+    //             <div className='col-md-2 center-items align-items-center'>
+    //                 <img className='bookFront' src={item.ruta} alt='portada-libro'/> 
+    //             </div>
+    //             <div className='col-md-8 libros'>
+    //                 <div className='col-md-12'>
+    //                     <label className='libros__titulo'>{item.titulo}</label>
+    //                 </div>
+    //                 <div className='col-md-12'>
+    //                     <label className='libros__autor'>{item.autor}</label>
+    //                 </div>
+    //                 <div className='col-md-12'>
+    //                     <p className='libros__descripcion'>{item.description}</p>
+    //                 </div>
+    //                 <div className='col-md-auto col-sm-12'>
+    //                     <span>{item.cantFavoritos} <FontIcon icon={heartIcon} color="var(--color-rojo)"/> </span>
+    //                     <span>{item.cantVistas} <FontIcon icon={eye} color="var(--colo-azul)"/> | </span>
+    //                     <label>{item.paginas} Pág. | {item.estado === "Disponible" ? <span className='disponible'>{item.estado} <FontIcon icon={circleCheck} color="var(--colo-verde)"/> </span> : <span className='enPrestamo'>{item.estado} <FontIcon icon={circleXmark} color="var(--colo-rojo)"/>  </span>}</label>
+    //                 </div>
+    //             </div>
+    //             <div className='col-md-2 center-items align-items-center'>
+    //                 <Link to={'/servioalquiler'}>
+    //                     <Button id={item.id} classStyle={"btnStyle"} valor={"¡Alquilar!"} onClick={() => almacenarData(item)} isDisabled={item.estado === "Disponible" ? false : true}/>
+    //                 </Link>
+    //             </div>
+    //         </div>            
+    //       </li>
+    //     ))}
+    //   </ul>
+    //   <div id='pagination'>
+    //     {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+    //         <Button classStyle={"btnStyle"} key={pageNumber} onClick={() => handlePaginationClick(pageNumber)} valor={pageNumber}/>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
